2f209b15cfb687f1990e6716240884479436a73c
Tristán <tristan.nouwens@student.gildeopleidingen.nl>
2026-02-06 19:33:09 +0100

    Added nextjs with prod docker.

---
+++ nextjs/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import {ReactNode} from 'react'
+
+export default function RootLayout({
+                                     children,
+                                   }: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en">
+    <head>
+      <title>Next.js App</title>
+    </head>
+    <body>{children}</body>
+    </html>
+  )
+}
